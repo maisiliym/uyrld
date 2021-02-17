96172b9f18ba325dc50b71e1b7f605b8a3e5b658
li <@li:maisiliym.uniks>
2021-02-17 15:51:03 +0000

    sobUyrldz && hob sintheticFleiks

--- flake.nix
+++ flake.nix
@@ -1,52 +1,81 @@
 {
-  description = "Uyrld";
+  description = "Uyrld - intial nix/uniks interface";
 
   outputs = registry@{ self, kor, lib, hob, hyraizyn }:
     let
+      /* Bootstrap - to be moved uniks-side */
+      kor = registry.kor.datom;
+      lib = registry.lib.datom;
+      hob = registry.hob.datom;
+      hyraizyn = registry.hyraizyn.datom;
+      krimyn = registry.hyraizyn.krimyn;
+
       inherit (builtins) hasAttr mapAttrs;
 
-      meik = indeks:
-        mapAttrs (n: flake: meikFlake flake) indeks;
-
-      meikSobUyrld = sobUyrld@{ lamdy, modz ? [ ], self }:
+      meikSobUyrld = sobUyrld@{ lamdy, modz, self }:
         let
           inherit (builtins) getAttr;
-
-          kor = registry.kor.datom;
           inherit (kor) mkLamdy optionalAttrs elem genAttrs;
 
-          hyraizyn = registry.hyraizyn.datom;
-          krimyn = registry.hyraizyn.krimyn;
+          Modz = [
+            "kor"
+            "lib"
+            "pkgs"
+            "mozPkgs"
+            "hob"
+            "pkdjz"
+            "hyraizyn"
+            "krimyn"
+            "uyrld"
+          ];
 
-          Modz = [ "kor" "lib" "pkgs" "hob" "krimyn" "hyraizyn" "mozPkgs" "uyrld" ];
           iuzMod = genAttrs Modz (n: (elem n modz));
 
-          klozyr = { inherit self; }
-            // optionalAttrs iuzMod.pkgs datom.pkgs
-            // optionalAttrs iuzMod.hob { hob = registry.hob.datom; }
-            // optionalAttrs iuzMod.uyrld datom
-            // optionalAttrs iuzMod.mozPkgs datom.mozPkgs
-            // optionalAttrs iuzMod.lib { lib = registry.lib.datom; }
+          klozyr = optionalAttrs iuzMod.kor { inherit kor; }
+            // optionalAttrs iuzMod.lib { inherit lib; }
+            // optionalAttrs iuzMod.pkgs uyrld.pkgs
+            // optionalAttrs iuzMod.mozPkgs uyrld.mozPkgs
+            // optionalAttrs iuzMod.hob { inherit hob; }
+            // optionalAttrs iuzMod.pkdjz { inherit (uyrld) pkdjz; }
             // optionalAttrs iuzMod.hyraizyn { inherit hyraizyn; }
-            // optionalAttrs iuzMod.kor { inherit kor; }
-            // optionalAttrs iuzMod.krimyn { inherit krimyn; };
+            // optionalAttrs iuzMod.krimyn { inherit krimyn; }
+            // optionalAttrs iuzMod.uyrld uyrld
+            // { inherit self; };
 
         in
         mkLamdy { inherit klozyr lamdy; };
 
-      meikFlake = flake@{ sobUyrld, ... }:
+      meikFleik = neim: fleik@{ ... }:
         let
-          self =
-            if (hasAttr "selfOvyraid" sobUyrld)
-            then sobUyrld.selfOvyraid else flake;
-        in
-        meikSobUyrld ({ inherit (sobUyrld) modz lamdy; } // { inherit self; });
+          priMeikSobUyrld = sobUyrld@{ modz ? [ ], lamdy, ... }:
+            let
+              izSelfHob = hasAttr "selfHob" sobUyrld &&
+                hasAttr sobUyrld.selfHob hob;
+              self =
+                if izSelfHob then hob.${sobUyrld.selfHob}
+                else sobUyrld.selfOvyraid or fleik;
+              fainylSobuyrld = { inherit self modz lamdy; };
+            in
+            meikSobUyrld fainylSobuyrld;
 
-      datom = meik hob.datom;
+          meik =
+            if (hasAttr "SobUyrldz" fleik)
+            then mapAttrs (n: su: priMeikSobUyrld su) fleik.SobUyrldz
+            else if (hasAttr "sobUyrld" fleik)
+            then priMeikSobUyrld fleik.sobUyrld
+            else fleik;
+        in
+        meik;
+
+      meik = indeks:
+        mapAttrs meikFleik indeks;
+
+      uyrld = meik hob;
 
     in
     {
-      inherit meik datom;
+      inherit meik;
+      datom = uyrld;
     };
 
 }
